4bc4bab64d4a0e555bb9adc60110fd3f89c07902
Jack Ellis <jack.ellis@godaddy.com>
2019-06-18 14:15:00 +0100

    fix: dist should be fully ES compatible

--- rollup/config.js
+++ rollup/config.js
@@ -1,29 +1,27 @@
 import babel from 'rollup-plugin-babel';
 import localResolve from 'rollup-plugin-local-resolve';
-import replace from 'rollup-plugin-replace';
-import uglify from 'rollup-plugin-uglify-es';
 
 export default {
   input: 'src/index.js',
   output: [
     {
-      file: 'dist/es/uteals.production.js',
+      file: 'dist/es/uteals.js',
       format: 'es',
     },
     {
-      file: 'dist/cjs/uteals.production.js',
+      file: 'dist/cjs/uteals.js',
       format: 'cjs',
     },
   ],
   plugins: [
     localResolve(),
-    replace({
-      'process.env.NODE_ENV': JSON.stringify('production'),
-    }),
     babel({
       exclude: 'node_modules/**',
       plugins: ['external-helpers'],
     }),
-    uglify(),
+  ],
+  external: [
+    'browser-cookies',
+    'ramda',
   ],
 };
